205b43d88abfe66fee2d80217015f01d347f530c
baizhe <919041098@qq.com>
2022-09-29 21:30:20 +0800

    save history

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,9 @@
 import {Square} from "./Square";
-import {useState} from "react";
-import {calculateWinner} from '../util'
 
-export const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
-  const winner = calculateWinner(squares)
-  const status = winner ? 'Winner is ' + winner :'Next player: ' + (xIsNext ? 'X' : 'O')
-  const handleClick = (i) => {
-    if(winner || squares[i]) return
-    const squares_copy = squares.slice()
-    squares_copy[i] = xIsNext ? 'X' : 'O'
-    setSquares(squares_copy)
-    setXIsNext(!xIsNext)
-  }
-  const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)}/>
-  }
+export const Board = (props) => {
+  const { squares, onClick } = props
+  const renderSquare = (i) => <Square value={squares[i]} onClick={() => onClick(i)}/>
   return <>
-    <div className="status">{status}</div>
     <div className="board-row">
       { renderSquare(0) }
       { renderSquare(1) }
@@ -36,26 +21,3 @@ export const Board = () => {
     </div>
   </>
 }
-
-// /**
-//  * 棋盘组件
-//  * @param {Array} squares 当前棋盘棋子状态
-//  * @param {Function} onClick 父组件的点击事件
-//  */
-// export const Board = (squares, onClick) => {
-//   const renderSquare = (start) => {
-//    return (
-//      <div className="board-row">{
-//        Array(start).map((val, index) => (
-//          <Square key={index} value={ squares[start + index] } onClick={() => onClick(start + index)} />
-//        ))
-//      }
-//      </div>
-//    )
-//   }
-//   return <>
-//     { renderSquare(0) }
-//     { renderSquare(1) }
-//     { renderSquare(2) }
-//   </>
-// }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,28 @@
 import {Board} from "./Board";
+import {useState} from "react";
+import {calculateWinner} from "../util";
 
 export const Game = () => {
+  const defaultSquares = [{ square: Array(9).fill(null)}]
+  const [history, setHistory] = useState(defaultSquares)
+  const [xIsNext, setXIsNext] = useState(true)
+  const current = history[history.length - 1]
+  const winner = calculateWinner(current.square)
+  const status = winner ? 'Winner is ' + winner :'Next player: ' + (xIsNext ? 'X' : 'O')
+  const handleClick = (i) => {
+    if(winner || current.square[i]) return
+    const square = current.square.slice()
+    square[i] = xIsNext ? 'X' : 'O'
+    setHistory(history.concat({ square }))
+    setXIsNext(!xIsNext)
+  }
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.square} onClick={(i)=> handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div className="status">{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
